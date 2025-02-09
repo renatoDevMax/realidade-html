d0c5310eb4eec045b0e20c58ea47471715042b20
renatoDevMax <renato.devmaximiano@gmail.com>
2025-02-08 22:46:17 -0300

    primeiro final 1.11

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,21 @@ export default function Home() {
         <div className="quadrado quadrado1"></div>
         <div className="quadrado quadrado2"></div>
         <div className="quadrado quadrado3"></div>
-        <div className="quadrado quadrado4"></div>
-        <div className="quadrado quadrado5"></div>
-        <div className="quadrado quadrado6"></div>
-        <div className="quadrado quadrado7"></div>
-        <div className="quadrado quadrado8"></div>
+
+        <div className="quadradoProduto">
+          <div className="nomeProduto">Hipoclorito de Sódio 10% </div>
+          <div className="descricaoProduto">
+            Hipoclorito de sódio 10% - 5 Litros
+          </div>
+          <div className="precoProduto">Hoje por R$ 29,50!</div>
+          <div className="bolaFundo1"></div>
+          <div className="bolaFundo2"></div>
+        </div>
+      </div>
+
+      <div className="telaBotoes w-full fixed bottom-0 left-0 z-[101] flex justify-between items-center">
+        <button className="btnCtrl btnRetornar">{"<"}</button>
+        <button className="btnCtrl btnAvancar">{">"}</button>
       </div>
     </main>
   );
