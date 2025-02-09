7304079cd2645e6740fe8b0fdb5f8dbad3d428d1
renatoDevMax <renato.devmaximiano@gmail.com>
2025-02-08 22:06:00 -0300

    primeiro final 1.10

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,17 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <ARScene />
+
+      <div className="telaQuadrados">
+        <div className="quadrado quadrado1"></div>
+        <div className="quadrado quadrado2"></div>
+        <div className="quadrado quadrado3"></div>
+        <div className="quadrado quadrado4"></div>
+        <div className="quadrado quadrado5"></div>
+        <div className="quadrado quadrado6"></div>
+        <div className="quadrado quadrado7"></div>
+        <div className="quadrado quadrado8"></div>
+      </div>
     </main>
   );
 }
